0efefc69bab1d24f91e537f91ec297e4ec026238
ArijSaaleh <arij.saleh@esprit.tn>
2023-10-23 21:11:42 +0100

    m

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,9 @@ const { authMiddleware } = require("./Middleware/authMiddleware");
 const taskRoute = require("./Routes/taskRoute");
 const userRoute = require("./Routes/userRoute");
 
+//express app
 var app = express();
+//database connection methods
 connectDB();
 
 //to be able to recognize ports between back & front
@@ -28,7 +30,15 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", taskRoute);
 app.use("/api/v1/users", userRoute);
-
+//handling other routes not included
+app.all("*", (req, res, next) => {
+  const err = new Error(`Can't find this route: ${req.originalUrl}`);
+  next(err.message);
+});
+// Global error handling middelware
+app.use((err, req, res, next) => {
+  res.status(400).json({ err });
+});
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
