c84d0a6a73e1ca1b9b74c228c916c57da87cfac2
ArijSaaleh <arij.saleh@esprit.tn>
2023-10-23 09:41:06 +0100

    index changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,33 @@
-const express = require("express");
-const { json } = require("express");
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const {authMiddleware} = require("./Middleware/authMiddleware")
-require('dotenv').config({ path: __dirname + '/.env' })
+require("dotenv").config({ path: __dirname + "/.env" });
 
-const connectDB = require("./database/connect")
+const express = require("express");
+
+const { json } = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { authMiddleware } = require("./Middleware/authMiddleware");
+
+const connectDB = require("./database/connect");
 const userRoute = require("./Routes/userRoute");
 const taskRoute = require("./Routes/taskRoute");
 
-connectDB();
 var app = express();
+connectDB();
+
 //to be able to recognize ports between back & front
 app.use(
-    cors({
-        credentials: true,
-        origin: ["http://localhost:4200"],
-    })
+  cors({
+    credentials: true,
+    origin: ["http://localhost:4200"],
+  })
 );
 // exchanging cookies
 app.use(cookieParser());
 app.use(json());
 app.use("/user", userRoute);
-app.listen(8000);
-module.exports=app;
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+module.exports = app;
